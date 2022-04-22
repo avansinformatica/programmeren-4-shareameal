da13f68252858a926e1bbfbbf50ad7fa03e01e2b
Robin Schellius <r.schellius@avans.nl>
2022-04-22 14:42:40 +0200

    Updated

--- database/inmemdb.js
+++ database/inmemdb.js
@@ -1,63 +1,69 @@
 // Deze variabelen worden niet geëxporteerd en kunnen dus niet
 // vanuit andere bestanden gewijzigd worden - alleen via de databasefuncties.
-const _moviedb = [];
-let id = 0;
+const _moviedb = []
+const timeout = 500 // msec
+let id = 0
 
 // Dit is het object dat geexporteerd wordt, en dus in andere JavaScript bestanden geïmporteerd kan worden, via require.
 module.exports = {
-  /**
-   * Maak een nieuwe movie aan in de database. De naam van de movie moet uniek zijn.
-   *
-   * @param {*} movie De movie die we toevoegen
-   * @param {*} callback De functie die ofwel een error, ofwel een resultaat teruggeeft.
-   */
-  createMovie(movie, callback) {
-    console.log("createMovie called");
-    // de naam van de movie moet uniek zijn.
-    // controleer daarom eerst of er al een movie met die naam in de _moviedb zit.
-    if (
-      movie &&
-      movie.name &&
-      _moviedb.filter((item) => item.name === movie.name).length > 0
-    ) {
-      const error = "A movie with this name already exists.";
-      console.log(error);
-      // roep de callback functie aan met error als resultaat, en result = undefined.
-      callback(error, undefined);
-    } else {
-      // voeg de id toe aan de movie, in de moveToAdd
-      const movieToAdd = {
-        id: id++,
-        ...movie,
-      };
-      _moviedb.push(movieToAdd);
-      // roep de callback aan, zonder error, maar met de nieuwe movie als result.
-      callback(undefined, movieToAdd);
-    }
-  },
+    /**
+     * Maak een nieuwe movie aan in de database. De naam van de movie moet uniek zijn.
+     *
+     * @param {*} movie De movie die we toevoegen
+     * @param {*} callback De functie die ofwel een error, ofwel een resultaat teruggeeft.
+     */
+    createMovie(movie, callback) {
+        console.log('createMovie called')
+        // we simuleren hier dat de database query 'lang' duurt, door een setTimeout toe te voegen.
+        setTimeout(() => {
+            // de naam van de movie moet uniek zijn.
+            // controleer daarom eerst of er al een movie met die naam in de _moviedb zit.
+            if (
+                movie &&
+                movie.name &&
+                _moviedb.filter((item) => item.name === movie.name).length > 0
+            ) {
+                const error = 'A movie with this name already exists.'
+                console.log(error)
+                // roep de callback functie aan met error als resultaat, en result = undefined.
+                callback(error, undefined)
+            } else {
+                // voeg de id toe aan de movie, in de moveToAdd
+                const movieToAdd = {
+                    id: id++,
+                    ...movie,
+                }
+                _moviedb.push(movieToAdd)
+                // roep de callback aan, zonder error, maar met de nieuwe movie als result.
+                callback(undefined, movieToAdd)
+            }
+        }, timeout)
+    },
 
-  /**
-   * Retourneer een lijst van alle movies.
-   * Om alle movies op te halen hebben we geen input param nodig,
-   * dus alleen een callback als parameter.
-   *
-   * @param {*} callback De functie die het resultaat retourneert.
-   */
-  listMovies(callback) {
-    console.log("lisMovies called");
-    // roep de callback aan, zonder error, maar met de hele moviedb als result.
-    callback(undefined, _moviedb);
-  },
+    /**
+     * Retourneer een lijst van alle movies.
+     * Om alle movies op te halen hebben we geen input param nodig,
+     * dus alleen een callback als parameter.
+     *
+     * @param {*} callback De functie die het resultaat retourneert.
+     */
+    listMovies(callback) {
+        console.log('lisMovies called')
+        setTimeout(() => {
+            // roep de callback aan, zonder error, maar met de hele moviedb als result.
+            callback(undefined, _moviedb)
+        }, timeout)
+    },
 
-  getMovieById() {
-    // zelf uitwerken
-  },
+    getMovieById() {
+        // zelf uitwerken
+    },
 
-  updateMovie() {
-    // zelf uitwerken
-  },
+    updateMovie() {
+        // zelf uitwerken
+    },
 
-  deleteMovie() {
-    // zelf uitwerken
-  },
-};
+    deleteMovie() {
+        // zelf uitwerken
+    },
+}
--- index.js
+++ index.js
@@ -1,90 +1,90 @@
-const express = require("express");
+const express = require('express')
 // De database importeren we nu uit het externe bestand.
-const database = require("./database/inmemdb");
+const database = require('./database/inmemdb')
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
 // const bodyParser = require("body-parser");
 // app.use(bodyParser.json());
 
 // express heeft tegenwoordig de bodyParser geïntegreerd.
 // hoeft niet meer apart geïnstalleerd en geïmporteerd te worden.
-app.use(express.json());
+app.use(express.json())
 
 // let database = [];
-let id = 0;
+let id = 0
 
-app.all("*", (req, res, next) => {
-  const method = req.method;
-  console.log(`Method ${method} is aangeroepen`);
-  next();
-});
+app.all('*', (req, res, next) => {
+    const method = req.method
+    console.log(`Method ${method} is aangeroepen`)
+    next()
+})
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    status: 200,
-    result: "Hello World",
-  });
-});
-
-app.post("/api/movie", (req, res) => {
-  // Hier gebruiken we nu de inmem database module om een movie toe te voegen.
-  // Optie: check vooraf of req.body wel de juiste properties/attribute bevat - gaan we later doen
-
-  // We geven in de createMovie functie de callbackfunctie mee. Die kan een error of een result teruggeven.
-  database.createMovie(req.body, (error, result) => {
-    if (error) {
-      console.log(`index.js: ${error}`);
-      res.status(401).json({
-        statusCode: 401,
-        error, // als de key en de value het zelfde kun je die zo vermelden. Hier staat eigenlijk: error: error
-      });
-    }
-    if (result) {
-      console.log(`index.js: movie successfully added!`);
-      res.status(200).json({
-        statusCode: 200,
-        result,
-      });
-    }
-  });
-});
-
-app.get("/api/movie/:movieId", (req, res, next) => {
-  const movieId = req.params.movieId;
-  console.log(`Movie met ID ${movieId} gezocht`);
-  let movie = database.filter((item) => item.id == movieId);
-  if (movie.length > 0) {
-    console.log(movie);
+app.get('/', (req, res) => {
     res.status(200).json({
-      status: 200,
-      result: movie,
-    });
-  } else {
+        status: 200,
+        result: 'Hello World',
+    })
+})
+
+app.post('/api/movie', (req, res) => {
+    // Hier gebruiken we nu de inmem database module om een movie toe te voegen.
+    // Optie: check vooraf of req.body wel de juiste properties/attribute bevat - gaan we later doen
+
+    // We geven in de createMovie functie de callbackfunctie mee. Die kan een error of een result teruggeven.
+    database.createMovie(req.body, (error, result) => {
+        if (error) {
+            console.log(`index.js: ${error}`)
+            res.status(401).json({
+                statusCode: 401,
+                error, // als de key en de value het zelfde kun je die zo vermelden. Hier staat eigenlijk: error: error
+            })
+        }
+        if (result) {
+            console.log(`index.js: movie successfully added!`)
+            res.status(200).json({
+                statusCode: 200,
+                result,
+            })
+        }
+    })
+})
+
+app.get('/api/movie/:movieId', (req, res, next) => {
+    const movieId = req.params.movieId
+    console.log(`Movie met ID ${movieId} gezocht`)
+    let movie = database.filter((item) => item.id == movieId)
+    if (movie.length > 0) {
+        console.log(movie)
+        res.status(200).json({
+            status: 200,
+            result: movie,
+        })
+    } else {
+        res.status(401).json({
+            status: 401,
+            result: `Movie with ID ${movieId} not found`,
+        })
+    }
+})
+
+app.get('/api/movie', (req, res, next) => {
+    database.listMovies((error, result) => {
+        res.status(200).json({
+            statusCode: 200,
+            result,
+        })
+    })
+})
+
+app.all('*', (req, res) => {
     res.status(401).json({
-      status: 401,
-      result: `Movie with ID ${movieId} not found`,
-    });
-  }
-});
-
-app.get("/api/movie", (req, res, next) => {
-  database.listMovies((error, result) => {
-    res.status(200).json({
-      statusCode: 200,
-      result,
-    });
-  });
-});
-
-app.all("*", (req, res) => {
-  res.status(401).json({
-    status: 401,
-    result: "End-point not found",
-  });
-});
+        status: 401,
+        result: 'End-point not found',
+    })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
