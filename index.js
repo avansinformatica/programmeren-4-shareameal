ff53238b853c76e1b7d7fdd8f9825f030ac7387c
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-04-23 23:04:42 +0200

    User requests

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 let movies = [];
-let id = 0;
+let movieID = 0;
 
 let users = [];
+let userID = 0;
 
 app.all("*", (req, res, next) => {
   const method = req.method;
@@ -26,9 +27,9 @@ app.get("/", (req, res) => {
 //Movies
 app.post("/api/movie", (req, res) => {
   let movie = req.body;
-  id++;
+  movieID++;
   movie = {
-    id,
+    movieID,
     ...movie,
   };
   console.log(movie);
@@ -42,7 +43,7 @@ app.post("/api/movie", (req, res) => {
 app.get("/api/movie/:movieId", (req, res, next) => {
   const movieId = req.params.movieId;
   console.log(`Movie met ID ${movieId} gezocht`);
-  let movie = movies.filter((item) => item.id == movieId);
+  let movie = movies.filter((item) => item.movieID == movieId);
   if (movie.length > 0) {
     console.log(movie);
     res.status(200).json({
@@ -67,7 +68,9 @@ app.get("/api/movie", (req, res, next) => {
 //Users
 app.post("/api/user", (req, res) => {
   let user = req.body;
+  userID++;
   user = {
+    userID,
     ...user,
   };
   console.log(user);
@@ -75,22 +78,53 @@ app.post("/api/user", (req, res) => {
 
   let validEmail;
 
-  users.forEach(u => {
-    if (u.getString("emailAdress") == user.getString("emailAdress")) {
-      res.status(401).json({
-        status: 401,
-        result: "Not an unique email",
-      });
-      validEmail = false;
-    } else {
-      validEmail = true;
+/*   for (const person in users) {
+    if (users.hasOwnProperty.call(users, person)) {
+      const u = object[person];
+      
+      if (u.getString("emailAdress") == user.getString("emailAdress")) {
+        res.status(401).json({
+          status: 401,
+          result: "Not an unique email",
+        });
+        validEmail = false;
+      } else {
+        validEmail = true;
+      }
     }
-  });
+  } */
+
+  validEmail = true;
 
   if (validEmail == true) {
     res.status(201).json({
       status: 201,
       result: users,
+    });
+  }
+});
+
+app.get("/api/user", (req, res, next) => {
+  res.status(200).json({
+    status: 200,
+    result: users,
+  });
+});
+
+app.get("/api/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+  console.log(`User met ID ${userId} gezocht`);
+  let user = users.filter((item) => item.userID == userId);
+  if (user.length > 0) {
+    console.log(user);
+    res.status(200).json({
+      status: 200,
+      result: user,
+    });
+  } else {
+    res.status(401).json({
+      status: 401,
+      result: `User with ID ${userId} not found`,
     });
   }
 });
