b4b48f70e51f78a6eeb40477592a67eebd4685c5
Robin Schellius <r.schellius@avans.nl>
2022-04-26 10:16:58 +0200

    Refactored routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 // De database importeren we nu uit het externe bestand.
 const database = require('./database/inmemdb')
+const movieRoutes = require('./src/routes/movie.routes')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -21,62 +22,8 @@ app.all('*', (req, res, next) => {
     next()
 })
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 200,
-        result: 'Hello World',
-    })
-})
-
-app.post('/api/movie', (req, res) => {
-    // Hier gebruiken we nu de inmem database module om een movie toe te voegen.
-    // Optie: check vooraf of req.body wel de juiste properties/attribute bevat - gaan we later doen
-
-    // We geven in de createMovie functie de callbackfunctie mee. Die kan een error of een result teruggeven.
-    database.createMovie(req.body, (error, result) => {
-        if (error) {
-            console.log(`index.js: ${error}`)
-            res.status(401).json({
-                statusCode: 401,
-                error, // als de key en de value het zelfde kun je die zo vermelden. Hier staat eigenlijk: error: error
-            })
-        }
-        if (result) {
-            console.log(`index.js: movie successfully added!`)
-            res.status(200).json({
-                statusCode: 200,
-                result,
-            })
-        }
-    })
-})
-
-app.get('/api/movie/:movieId', (req, res, next) => {
-    const movieId = req.params.movieId
-    console.log(`Movie met ID ${movieId} gezocht`)
-    let movie = database.filter((item) => item.id == movieId)
-    if (movie.length > 0) {
-        console.log(movie)
-        res.status(200).json({
-            status: 200,
-            result: movie,
-        })
-    } else {
-        res.status(401).json({
-            status: 401,
-            result: `Movie with ID ${movieId} not found`,
-        })
-    }
-})
-
-app.get('/api/movie', (req, res, next) => {
-    database.listMovies((error, result) => {
-        res.status(200).json({
-            statusCode: 200,
-            result,
-        })
-    })
-})
+// Alle movieRoutes beginnen met /api
+app.use('/api', movieRoutes)
 
 app.all('*', (req, res) => {
     res.status(401).json({
