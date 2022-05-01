4cde65844c6f621a2fef29c6d2047199a9fc180a
Robin Schellius <r.schellius@avans.nl>
2022-05-01 15:17:33 +0200

    GitHub workflow testing

---
+++ share-a-meal.create.sql
@@ -0,0 +1,18 @@
+-- 
+-- Gebruik dit script om je lokale database, en je test-database te maken.
+-- LET OP: je moet als root ingelogd zijn om dit script uit te kunnen voeren.
+-- Dit script creëert de database, de user, en opent de nieuwe database.
+-- Daarna kun je het share-a-meal.sql script importeren.
+-- In de connection settings gebruik je dan de nieuwe database, de user en het password.
+-- 
+DROP DATABASE IF EXISTS `share-a-meal`;
+CREATE DATABASE `share-a-meal`;
+-- DROP DATABASE IF EXISTS `share-a-meal_testdb`;
+-- CREATE DATABASE `share-a-meal_testdb`;
+-- share-a-meal-user aanmaken
+CREATE USER 'share-a-meal-user'@'localhost' IDENTIFIED BY 'secret';
+CREATE USER 'share-a-meal-user'@'%' IDENTIFIED BY 'secret';
+-- geef rechten aan deze user
+GRANT SELECT, INSERT, DELETE, UPDATE ON `share-a-meal`.* TO 'share-a-meal-user'@'%';
+
+USE `share-a-meal`;
