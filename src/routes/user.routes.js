5248e5500fd0486366652260510a10fc9c5abd3f
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-05-11 15:10:59 +0200

    8 tests passing

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,7 +11,7 @@ router.get("/", (req, res) => {
   });
 
 //UC-201 Add a user
-router.post("/api/user", userController.validateEmail, userController.validateUser, userController.addUser);
+router.post("/api/user", userController.validateUser, userController.addUser);
 
 //UC-202 Get all users
 router.get("/api/user", userController.getAllUsers);
@@ -20,12 +20,12 @@ router.get("/api/user", userController.getAllUsers);
 router.get("/api/user/profile", userController.getUserProfile);
 
 //UC-204 Get info of specific user 
-router.get("/api/user/:userId", userController.getUserById);
+router.get("/api/user/:userId", userController.userExists, userController.getUserById);
 
 //UC-205 Update a user
-router.put("/api/user/:id", userController.canUpdate, userController.validateUser, userController.updateUser);
+router.put("/api/user/:id", userController.userExists, userController.validateUser, userController.updateUser);
 
 //UC-206 Delete a user
-router.delete("/api/user/:userId", userController.deleteUser);
+router.delete("/api/user/:userId", userController.userExists, userController.deleteUser);
 
 module.exports = router;
--- test/integration/user.controller.test.js
+++ test/integration/user.controller.test.js
@@ -1,198 +1,334 @@
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const server = require('../../index')
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../../index");
 
 let database = [];
 
 chai.should();
 chai.use(chaiHttp);
 
-describe('Manage users /api/user', () => {
-    describe('TC-201-1 add a user, all properties are required except id', () => {
-        beforeEach((done) => {
-            database = [];
-            done();
+let userId;
+
+describe("Manage users /api/user", () => {
+  describe("TC-201-1 add a user, all properties are required except id", () => {
+    it("First name missing or invalid, return valid error", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          //First name missing
+          lastName: "Doe",
+          street: "Lovensdijkstraat 61",
+          city: "Breda",
+          emailAdress: "j.doe@server.com",
+          password: "secret",
+          phoneNumber: "06 12425475",
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(400);
+          result.should.be
+            .a("string")
+            .that.equals("First name must be a string");
+          done();
         });
+    });
+    it("Last name missing or invalid, return valid error", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          //Last name missing
+          firstName: "John",
+          street: "Lovensdijkstraat 61",
+          city: "Breda",
+          emailAdress: "j.doe@server.com",
+          password: "secret",
+          phoneNumber: "06 12425475",
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(400);
+          result.should.be
+            .a("string")
+            .that.equals("Last name must be a string");
+          done();
+        });
+    });
+    it("Street missing or invalid, return valid error", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          //Street missing
+          firstName: "John",
+          lastName: "Doe",
+          city: "Breda",
+          emailAdress: "j.doe@server.com",
+          password: "secret",
+          phoneNumber: "06 12425475",
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(400);
+          result.should.be.a("string").that.equals("Street must be a string");
+          done();
+        });
+    });
+    it("City missing or invalid, return valid error", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          //City missing
+          firstName: "John",
+          lastName: "Doe",
+          street: "Lovensdijkstraat 61",
+          emailAdress: "j.doe@server.com",
+          password: "secret",
+          phoneNumber: "06 12425475",
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(400);
+          result.should.be.a("string").that.equals("City must be a string");
+          done();
+        });
+    });
+    it("Email missing or invalid, return valid error", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          //Email missing
+          firstName: "John",
+          lastName: "Doe",
+          street: "Lovensdijkstraat 61",
+          city: "Breda",
+          password: "secret",
+          phoneNumber: "06 12425475",
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(400);
+          result.should.be.a("string").that.equals("Email must be a string");
+          done();
+        });
+    });
+    it("Password missing or invalid, return valid error", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          //Password missing
+          firstName: "John",
+          lastName: "Doe",
+          street: "Lovensdijkstraat 61",
+          city: "Breda",
+          emailAdress: "j.doe@server.com",
+          phoneNumber: "06 12425475",
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(400);
+          result.should.be.a("string").that.equals("Password must be a string");
+          done();
+        });
+    });
+                            /*         it('Phone number missing or invalid, return valid error', (done) => {
+                                    chai.request(server)
+                                        .post('/api/user')
+                                        .send({
+                                            //Phone number missing
+                                            firstName: "John",
+                                            lastName: "Doe",
+                                            street: "Lovensdijkstraat 61",
+                                            city: "Breda",
+                                            emailAdress: "j.doe@server.com",
+                                            password: "secret",
+                                        })
+                                        .end((err, res) => {
+                                            res.should.be.an('object')
+                                            let {status, result} = res.body;
+                                            status.should.equals(400)
+                                            result.should.be.a('string').that.equals('Phonenumber must be a string');
+                                            done();
+                                        })
+                                }) */
 
-        it('First name missing or invalid, return valid error', (done) => {
-            chai.request(server)
-                .post('/api/user')
-                .send({
-                    //First name missing
-                    lastName: "Doe",
-                    street: "Lovensdijkstraat 61",
-                    city: "Breda",
-                    emailAdress: "j.doe@server.com",
-                    password: "secret",
-                    phoneNumber: "06 12425475",
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let {status, result} = res.body;
-                    status.should.equals(400)
-                    result.should.be.a('string').that.equals('First name must be a string');
-                    done();
-                })
+    it("TC-201-4 User already exists", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          firstName: "Jan",
+          lastName: "Jansen",
+          isActive: true,
+          emailAdress: "m.vandullemen@server.nl", //email that exists
+          password: "secretpassword",
+          phoneNumber: "0612345678",
+          roles: "editor,guest",
+          street: "PC Hooftstraat",
+          city: "Amsterdam",
         })
-        it('Last name missing or invalid, return valid error', (done) => {
-            chai.request(server)
-                .post('/api/user')
-                .send({
-                    //Last name missing
-                    firstName: "John",
-                    street: "Lovensdijkstraat 61",
-                    city: "Breda",
-                    emailAdress: "j.doe@server.com",
-                    password: "secret",
-                    phoneNumber: "06 12425475",
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let {status, result} = res.body;
-                    status.should.equals(400)
-                    result.should.be.a('string').that.equals('Last name must be a string');
-                    done();
-                })
-        })
-        it('Street missing or invalid, return valid error', (done) => {
-            chai.request(server)
-                .post('/api/user')
-                .send({
-                    //Street missing
-                    firstName: "John",
-                    lastName: "Doe",
-                    city: "Breda",
-                    emailAdress: "j.doe@server.com",
-                    password: "secret",
-                    phoneNumber: "06 12425475",
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let {status, result} = res.body;
-                    status.should.equals(400)
-                    result.should.be.a('string').that.equals('Street must be a string');
-                    done();
-                })
-        })
-        it('City missing or invalid, return valid error', (done) => {
-            chai.request(server)
-                .post('/api/user')
-                .send({
-                    //City missing
-                    firstName: "John",
-                    lastName: "Doe",
-                    street: "Lovensdijkstraat 61",
-                    emailAdress: "j.doe@server.com",
-                    password: "secret",
-                    phoneNumber: "06 12425475",
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let {status, result} = res.body;
-                    status.should.equals(400)
-                    result.should.be.a('string').that.equals('City must be a string');
-                    done();
-                })
-        })
-        it('Email missing or invalid, return valid error', (done) => {
-            chai.request(server)
-                .post('/api/user')
-                .send({
-                    //Email missing
-                    firstName: "John",
-                    lastName: "Doe",
-                    street: "Lovensdijkstraat 61",
-                    city: "Breda",
-                    password: "secret",
-                    phoneNumber: "06 12425475",
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let {status, result} = res.body;
-                    status.should.equals(400)
-                    result.should.be.a('string').that.equals('Email must be a string');
-                    done();
-                })
-        })
-        it('Password missing or invalid, return valid error', (done) => {
-            chai.request(server)
-                .post('/api/user')
-                .send({
-                    //Password missing
-                    firstName: "John",
-                    lastName: "Doe",
-                    street: "Lovensdijkstraat 61",
-                    city: "Breda",
-                    emailAdress: "j.doe@server.com",
-                    phoneNumber: "06 12425475",
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let {status, result} = res.body;
-                    status.should.equals(400)
-                    result.should.be.a('string').that.equals('Password must be a string');
-                    done();
-                })
-        })
-/*         it('Phone number missing or invalid, return valid error', (done) => {
-            chai.request(server)
-                .post('/api/user')
-                .send({
-                    //Phone number missing
-                    firstName: "John",
-                    lastName: "Doe",
-                    street: "Lovensdijkstraat 61",
-                    city: "Breda",
-                    emailAdress: "j.doe@server.com",
-                    password: "secret",
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let {status, result} = res.body;
-                    status.should.equals(400)
-                    result.should.be.a('string').that.equals('Phonenumber must be a string');
-                    done();
-                })
-        }) */
+        .end((req, res) => {
+          res.should.be.an("object");
+          let { status } = res.body;
+          status.should.equals(409);
+          done();
+        });
+    });
 
-        it('TC_201-5 email adress is unique in database', (done) => {
-            chai.request(server)
-            .post('/api/user')
+    it("TC-201-5 User has been registered succesfully", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          firstName: "Jan",
+          lastName: "Jansen",
+          isActive: true,
+          emailAdress: "janjansen@server.nl", //email that exists
+          password: "secretpassword",
+          phoneNumber: "0612345678",
+          roles: "editor,guest",
+          street: "PC Hooftstraat",
+          city: "Amsterdam",
+        })
+        .end((req, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+
+          //store id for delete test
+          userId = result.id;
+
+          status.should.equals(201);
+          done();
+        });
+    });
+  });
+
+  // UC-204 Get user details
+  describe("UC-204 Get user details", () => {
+    // it("TC-204-1 Invalid token");
+    it("TC-204-2 User ID doesn't exist", (done) => {
+      chai
+        .request(server)
+        .get("/api/user/0")
+        .end((req, res) => {
+          let { status } = res.body;
+          status.should.equals(404);
+          done();
+        });
+    });
+    it("TC-204-3 User ID exists", (done) => {
+      chai
+        .request(server)
+        .get("/api/user/" + userId)
+        .end((req, res) => {
+          let { status } = res.body;
+          status.should.equals(200);
+          done();
+        });
+    });
+  });
+
+  describe("UC-205 Update user", () => {
+    it("TC-205-1 Email missing", (done) => {
+      chai
+        .request(server)
+        .put("/api/user/1")
+        .send({
+          firstName: "Jet",
+          lastName: "Jansen",
+          isActive: true,
+          // missing email should end up failing
+          password: "secret",
+          phoneNumber: "0612345678",
+          roles: "editor,guest",
+          street: "Hopstraat",
+          city: "Amsterdam",
+        })
+        .end((req, res) => {
+          let { status, message } = res.body;
+          status.should.equals(400);
+          message.should.be
+            .a("string")
+            .that.equals("Email must be a string.");
+          done();
+        });
+    });
+
+    it("TC-205-4 User ID doesn't exist", (done) => {
+        chai.request(server)
+            .put("/api/user/0")
             .send({
-                //Phone number missing
-                firstName: "John",
-                lastName: "Doe",
-                street: "Lovensdijkstraat 61",
-                city: "Breda",
-                emailAdress: "j.doe@server.com",
-                password: "secret",
-                phoneNumber: "06 12425475",
+                firstName: "Jan",
+                lastName: "Jansen",
+                isActive: true,
+                emailAdress: "janjansen@server.nl",
+                password: "secretpassword",
+                phoneNumber: "0612345678",
+                roles: "editor,guest",
+                street: "PC Hooftstraat",
+                city: "Amsterdam",
             })
-            .end((err, res) => {
-                res.should.be.an('object')
-                let {status, result} = res.body;
-                status.should.equals(201)
-            })
-
-            chai.request(server)
-            .post('/api/user')
-            .send({
-                //Phone number missing
-                firstName: "John",
-                lastName: "Doe",
-                street: "Lovensdijkstraat 61",
-                city: "Breda",
-                emailAdress: "j.doe@server.com",
-                password: "secret",
-                phoneNumber: "06 12425475",
-            })
-            .end((err, res) => {
-                res.should.be.an('object')
-                let {status, result} = res.body;
-                status.should.equals(400)
-                result.should.be.a('string').that.equals('Email is already in use');
+            .end((req, res) => {
+                let { status } = res.body;
+                status.should.equals(400);
                 done();
+            });
+    });
+
+    it("TC-205-6 User updated succesfully", (done) => {
+        chai.request(server)
+            .put("/api/user/" + userId)
+            .send({
+                firstName: "Jan",
+                lastName: "Joker",
+                isActive: true,
+                emailAdress: `janjoker@gmail.com`,
+                password: "secret",
+                phoneNumber: "0612345678",
+                roles: "editor,guest",
+                street: "Hopstraat",
+                city: "Amsterdam",
             })
-        })
-    })
-})
+            .end((req, res) => {
+                let { status } = res.body;
+                status.should.equals(200);
+                done();
+            });
+    });
+  });
+
+  describe("UC-206 Delete user", () => {
+    it("TC-206-1 User doesn't exist", (done) => {
+        chai.request(server)
+            .delete("/api/user/0")
+            .end((req, res) => {
+                let { status } = res.body;
+                status.should.equals(400);
+                done();
+            });
+    });
+
+    it("TC-206-4 User deleted successfully", (done) => {
+        chai.request(server)
+            .delete("/api/user/" + userId)
+            .end((req, res) => {
+                let { status } = res.body;
+
+                status.should.equals(200);
+                res.body.should.have.property("result");
+
+                done();
+            });
+    });
+});
+});
